894ee871735de8493378a9ccc7871b61cd3e444e
Quincy-Bee <quincybee@icloud.com>
2026-07-15 16:06:44 -0400

    router

--- routes/grades.js
+++ routes/grades.js
@@ -1,4 +1,15 @@
 import express from "express";
-import grade from "../models/Grade";
+import Grade from "../models/Grade.js";
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const grades = await Grade.find();
+        res.json(grades);
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+});
